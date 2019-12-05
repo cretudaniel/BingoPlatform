03246e46092cddb1ced8fb76c168333180e48b0b
DanielCretu <bsdipermedia@gmail.com>
2019-12-05 21:48:38 +0100

    The php script to change the image of a user profile

--- adauga-imagine.php
+++ adauga-imagine.php
@@ -1,15 +1,37 @@
 <?php
 
 include 'connection.php';
-
-$imagine_name= $_FILES['imagine']['name'];
-
-$query = mysqli_query($db, "INSERT INTO user(photo) VALUES ('$imagine_name')");
-if(mysqli_insert_id($db)){
-
-  move_uploaded_file($_FILES['imagine']['tmp_name'], "imagini/" . $imagine_nume);
-  echo "Am adaugat imaginea";
+session_start();
+if (isset($_SESSION['username'])) {
+  $username=$_SESSION['username'];
 }
-else{
-  echo mysqli_error($db);
+else {
+   header("Location: login.php");
 }
+$ImageName = $_FILES['imagine']['name'];
+$fileElementName = 'imagine';
+$path = 'imagini/';
+$location = $path . $_FILES['imagine']['name'];
+move_uploaded_file($_FILES['imagine']['tmp_name'], $location);
+// $uploaddir = '/webapp/imagini';
+// $uploadfile = $uploaddir . $_FILES['imagine']['name'];
+
+
+$query = mysqli_query($db, "SELECT * FROM user WHERE username='$username'");
+if (mysqli_num_rows($query)) {
+    $sql="UPDATE user SET photo='$ImageName' WHERE username='$username'";
+    if (mysqli_query($db, $sql)) {
+        //  echo "Record updated successfully";
+      } else {
+          echo "Error updating record: " . mysqli_error($conn);
+        }
+}
+
+//GOOD CODE
+// $query = mysqli_query($db, "INSERT INTO user(photo) VALUES ('$ImageName')");
+//  if(mysqli_insert_id($db)){
+//    echo "good job";
+//  }
+//  else{
+//    echo mysqli_error($db);
+//  }
